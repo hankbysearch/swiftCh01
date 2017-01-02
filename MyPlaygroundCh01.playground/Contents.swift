e6c16067c236f12b57cde29a8c8c8d6082140df5
kuo <hankbysearch@gmail.com>
2017-01-02 13:53:55 +0800

    基本知識

---
+++ MyPlaygroundCh01.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*
+    此單元是基本知識
+ */
+
+/* 印出文字*/
+var str = "Hello, playground"
+
+print(str);
+print("hello");
+
+/* 變數及常數 */
+// 使用 let 宣告常數 name
+let name = "Hank"
+
+// 使用 var 宣告變數 price
+var price = 300
+
+// 接著就可以使用這個宣告過的常數 用來印出名字
+print(name)
+
+/* 命名規則 
+ 小駝峰式命名法(lower camel case)
+ 
+ 第一個單字以小寫字母開始，第二個及之後的單字的首字母則使用大寫
+    像是：firstName、somePerson。
+ Swift 中通常命名常數、變數、函式、屬性、方法及下標時
+    會使用小駝峰式命名法。
+ 
+ 大駝峰式命名法(upper camel case)
+ 
+ 每個單字的首字母都使用大寫字母，像是LastName、SomeClass。
+ Swift 中通常命名列舉、結構、類別、擴展、協定及其他自定義型別時
+    會使用大駝峰式命名法。
+ */
+
+let score = 100
+let myFullName = "Hank"
+
+
+
